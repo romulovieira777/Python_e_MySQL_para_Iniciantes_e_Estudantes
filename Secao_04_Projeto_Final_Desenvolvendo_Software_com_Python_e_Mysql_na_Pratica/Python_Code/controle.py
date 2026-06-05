cc0e7861981c20a69b39456affebc0ad0549db27
RomuloVieira777 <romulo.vieira777@gmail.com>
2026-06-05 13:06:43 -0300

    Projeto Final - Desenvolvendo Software com Python e MySQL na Prática

--- Secao_04_Projeto_Final_Desenvolvendo_Software_com_Python_e_Mysql_na_Pratica/Python_Code/controle.py
+++ Secao_04_Projeto_Final_Desenvolvendo_Software_com_Python_e_Mysql_na_Pratica/Python_Code/controle.py
@@ -9,6 +9,25 @@ conexao = mysql.connector.connect(
 )
 
 
+def editar():
+    dados = lista.tableWidget.currentRow()
+
+
+def lista():
+    lista.show()
+    cursor = conexao.cursor()
+    comando_SQL = "SELECT * FROM produtos"
+    cursor.execute(comando_SQL)
+    leitura_banco = cursor.fetchall()
+
+    lista.tableWidget.setRowCount(len(leitura_banco))
+    lista.tableWidget.setColumnCount(4)
+
+    for i in range(0, len(leitura_banco)):
+        for j in range(0, 4):
+            lista.tableWidget.setItem(i, j, QtWidgets.QTableWidgetItem(str(leitura_banco[i][j])))
+
+
 def inserir():
     produto = formulario.txtProduto.text()
     preco = formulario.txtPreco.text()
@@ -30,6 +49,9 @@ def inserir():
 app = QtWidgets.QApplication([])
 formulario = uic.loadUi("formulario.ui")
 formulario.btnCadastrar.clicked.connect(inserir)
+formulario.btnRelatorio.clicked.connect(lista)
+lista = uic.loadUi("lista.ui")
+editar = uic.loadUi("editar.ui")
 
 formulario.show()
 app.exec()
